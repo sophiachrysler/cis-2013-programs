50ad70944d5091c97e764415a9bfb0ff2d175b09
sophiachrysler <70539424+sophiachrysler@users.noreply.github.com>
2020-09-14 14:54:07 -0500

    Add files via upload

---
+++ mpg.js
@@ -0,0 +1,5 @@
+var floatMiles, floatGallons, floatMPG;
+     floatMiles = prompt("Enter miles driven");
+     floatGallons = prompt("Enter gallons of gas used");
+     floatMPG = floatMiles/floatGallons;
+     alert("Miles per gallon = " + mpg);
