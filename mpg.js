3e18fb1eb56ebe3e449bbef329e06b3c724a10a2
sophiachrysler <70539424+sophiachrysler@users.noreply.github.com>
2020-09-17 14:50:37 -0500

    Update mpg.js

--- mpg.js
+++ mpg.js
@@ -2,4 +2,4 @@ var floatMiles, floatGallons, floatMPG;
      floatMiles = parseFloat(prompt("Enter miles driven"));
      floatGallons = parseFloat(prompt("Enter gallons of gas used"));
      floatMPG = floatMiles/floatGallons;
-     alert("Miles per gallon = " + floatMPG);
+     alert("Miles per gallon = " + floatMPG).toFixed(2);
